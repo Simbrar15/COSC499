331b46279e97a5b0333a98a8875115cf456a019c
Simbrar15 <simbrar15@gmail.com>
2022-09-16 16:36:40 -0700

    I created a simple complex number calculator. For now it can only do addition.

---
+++ Calc/ComplexCalc.java
@@ -0,0 +1,16 @@
+public class ComplexCalc {
+    public static void main (String[] args) {
+
+        ComplexNumber x = new ComplexNumber(5,3);
+        ComplexNumber y = new ComplexNumber(5,3);
+
+
+    }
+    
+}
+
+
+
+
+
+
