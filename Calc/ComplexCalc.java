521a331c7c5d48e11218fa56e66a2a3027582599
Simbrar15 <simbrar15@gmail.com>
2022-09-16 22:56:25 -0700

    Made a few small changes
    Added an output line.

--- Calc/ComplexCalc.java
+++ Calc/ComplexCalc.java
@@ -1,10 +1,6 @@
 public class ComplexCalc {
     public static void main (String[] args) {
 
-        ComplexNumber x = new ComplexNumber(5,3);
-        ComplexNumber y = new ComplexNumber(5,3);
-
-
     }
     
 }
--- Calc/ComplexNumber.java
+++ Calc/ComplexNumber.java
@@ -18,11 +18,14 @@ public class ComplexNumber {
     public void add(double real, double imaginary){
         this.real += real;
         this.imaginary += imaginary;
+        System.out.println("The sum is: " + this.real + " + " + this.imaginary);
+
     }
     
     public void add(ComplexNumber complexNumber){
         this.real += complexNumber.real;
         this.imaginary += complexNumber.imaginary;
+        System.out.println("The sum is: " + this.real + " + " + this.imaginary);
     }
     
 }
